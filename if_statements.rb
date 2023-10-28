49654a7f99f3bf95c45ca0c1c7837e35866d83eb
Nico Shanstrom <nicoshanstrom@gmail.com>
2023-10-27 21:21:47 -1000

    Remove space again

--- if_statements.rb
+++ if_statements.rb
@@ -72,8 +72,8 @@ puts "I don't have enough money for a gumball"
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour =2
-has_sauce =true
+cups_of_flour=2
+has_sauce=true
 
 if cups_of_flour=1 && has_sauce=true
   p "I cannot make pizza"
